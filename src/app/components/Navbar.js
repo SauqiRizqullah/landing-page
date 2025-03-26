a53accaeb5cc1d1a7ca47c13260bd89160d4bfbe
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-03-26 10:31:59 +0700

    [ADD] Initiate Landing Page

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,21 @@
+
+
+export default function Navbar(){
+    return (
+        <navbar>
+            <div className="flex justify-around bg-blue-custom pt-2 pb-1">
+                <div className="roboto text-slate-50">
+                    <span>TeraFlow</span>
+                </div>
+                <div className="roboto flex justify-around gap-7 text-xs">
+                    <button className="cursor-pointer text-slate-50">HOME</button>
+                    <button className="cursor-pointer text-slate-50">FEATURES</button>
+                    <button className="cursor-pointer text-slate-50">CONTACT</button>
+                    <div className="bg-pink-lg py-1 px-2 rounded-full">
+                        <button className="cursor-pointer text-slate-50">ORDER NOW</button>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,102 +2,73 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <div className="bg-blue-custom pt-20 text-slate-50 text-4xl roboto text-center pb-4 font-semibold">
+        <span>DJI Phantom 4 RTK</span>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-col bg-[url('/img/svg.png')] bg-cover bg-center h-88 w-full items-center">
+        <Image
+          src="/img/phantom-2.png"
+          alt="phantom"
+          height={500}
+          width={500}
+          className="pt-5"
+        />
+      </div>
+
+      <div className="mt-28 grid grid-cols-3 ">
+        {/* Kiri */}
+        <div className="col-span-2 flex flex-col mr-32">
+          <div className="text-blue-900 text-2xl roboto font-semibold text-right">
+            <span>What made this outstanding?</span>
+          </div>
+          <div className="mt-5">
+            <div className="flex flex-row gap-4 justify-self-end">
+              <div className="roboto text-lg text-blue-900 font-semibold">
+                <span>Real Time Kinematic</span>
+              </div>
+              
+              <Image
+                src="/img/target.png"
+                alt="target"
+                width={30}
+                height={30}
+              />
+            </div>
+            <div className="text-sm ml-40 justify-self-end text-gray-600 mt-1 font-semibold roboto">
+              <span>
+                Generating more precision geospatial data than using standard
+                GPS
+              </span>
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="flex flex-row gap-4 justify-self-end">
+              <div className="roboto text-lg text-blue-900 font-semibold">
+                <span>Real Time Kinematic</span>
+              </div>
+              
+              <Image
+                src="/img/target.png"
+                alt="target"
+                width={30}
+                height={30}
+              />
+            </div>
+            <div className="text-sm ml-40 justify-self-end text-gray-600 mt-1 font-semibold roboto">
+              <span>
+                Generating more precision geospatial data than using standard
+                GPS
+              </span>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {/* Kanan */}
+        <div>
+          <Image src="/img/why.png" alt="why" width={200} height={200} />
+        </div>
+      </div>
+    </main>
   );
 }
