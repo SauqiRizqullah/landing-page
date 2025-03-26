6672a8d8b075e48e3dcf11972a9a3de94287cb08
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-03-26 21:42:07 +0700

    [FIX] Responsive in Main Content

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,22 +1,22 @@
-
+import Image from "next/image"
 
 export default function Navbar(){
     return (
         <navbar>
-            <div className="flex lg:justify-around justify-between bg-blue-custom pt-2 pb-1 lg:pl-0 pl-16">
-                <div className="roboto text-slate-50 ml-4">
+            <div className="flex lg:justify-around justify-between bg-blue-custom pt-2 pb-1 ">
+                <div className="roboto text-slate-50 ml-4 lg:pl-0 md:pl-28 pl-20 lg:text-md md:text-md text-sm">
                     <span>TeraFlow</span>
                 </div>
                 <div className="roboto lg:flex hidden justify-around gap-7 text-xs">
-                    <button className="cursor-pointer text-slate-50">HOME</button>
-                    <button className="cursor-pointer text-slate-50">FEATURES</button>
-                    <button className="cursor-pointer text-slate-50">CONTACT</button>
-                    <div className="bg-pink-lg py-1 px-2 rounded-full">
-                        <button className="cursor-pointer text-slate-50">ORDER NOW</button>
+                    <button className="cursor-pointer text-slate-50 transition duration-150 hover:scale-105">HOME</button>
+                    <button className="cursor-pointer text-slate-50 transition duration-150 hover:scale-105">FEATURES</button>
+                    <button className="cursor-pointer text-slate-50 transition duration-150 hover:scale-105">CONTACT</button>
+                    <div className="bg-pink-lg py-1 px-2 rounded-full self-center transition duration-150 hover:scale-105 hover:bg-slate-50 hover:text-pink-800 text-slate-50">
+                        <button className="cursor-pointer ">ORDER NOW</button>
                     </div>
                 </div>
-                <div className="">
-
+                <div className="lg:hidden pr-24 cursor-pointer">
+                    <Image src="/img/menu-hamburger.png" alt="MENU" height={25} width={25}/>
                 </div>
             </div>
         </navbar>
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="bg-blue-custom pt-20 text-slate-50 text-4xl roboto text-center pb-4 font-semibold">
+      <div className="bg-blue-custom pt-20 text-slate-50 lg:text-4xl md:text-3xl text-2xl roboto text-center pb-4 font-semibold">
         <span className="mx-6">DJI Phantom 4 RTK</span>
       </div>
 
@@ -13,12 +13,12 @@ export default function Home() {
           alt="phantom"
           height={352}
           width={500}
-          className="pt-5 mx-6 w-[400px] h-[282px] md:w-[500px] md:h-[352px]"
+          className="pt-5 mx-6 w-[350px] h-[246px] md:w-[400px] md:h-[282px] lg:w-[500px] lg:h-[352px]"
         />
       </div>
 
-      <div className="text-blue-900 text-2xl roboto font-semibold text-center mt-28">
-        <span>What made this outstanding?</span>
+      <div className="text-blue-900 md:text-2xl text-xl roboto font-semibold text-center md:mt-28 mt-32">
+        <span>Our Secret Weapon</span>
       </div>
 
       <div className="mt-12 lg:grid hidden grid-cols-5">
@@ -39,7 +39,7 @@ export default function Home() {
             </div>
             <div className="grid grid-cols-3">
               <div></div>
-              <div className="col-span-2 text-sm text-gray-600 mt-1 font-semibold roboto text-right">
+              <div className="col-span-2 text-sm text-gray-600 mt-4 font-semibold roboto text-right">
                 <span>
                   Generating more precision geospatial data than using standard
                   GPS.
@@ -63,7 +63,7 @@ export default function Home() {
             </div>
             <div className="grid grid-cols-3">
               <div></div>
-              <div className="col-span-2 text-sm text-gray-600 mt-1 font-semibold roboto text-right">
+              <div className="col-span-2 text-sm text-gray-600 mt-4 font-semibold roboto text-right">
                 <span>
                   By combining with RTK, it could producing a pin-sharp photo.
                 </span>
@@ -90,7 +90,7 @@ export default function Home() {
                 <span>Wide Flight Time and Range</span>
               </div>
             </div>
-            <div className="text-sm mr-40 text-gray-600 mt-1 font-semibold roboto">
+            <div className="text-sm mr-40 text-gray-600 mt-4 font-semibold roboto">
               <span>
                 Suitable for huge project by flying within 1km<sup>2</sup> in 30
                 minutes with high accuration.
@@ -109,7 +109,7 @@ export default function Home() {
                 <span>Sensor of Obstacle Avoidance</span>
               </div>
             </div>
-            <div className="text-sm mr-40 justify-self-start text-gray-600 mt-1 font-semibold roboto">
+            <div className="text-sm mr-40 justify-self-start text-gray-600 mt-4 font-semibold roboto">
               <span>
                 With 10 - 20 Hz detection speed, allowing to prevent from an
                 unpredictable obstacle while flying.
@@ -120,17 +120,17 @@ export default function Home() {
       </div>
 
       {/* Pro's mobile / tablet section */}
-      <div className="justify-self-center lg:hidden mt-6 mx-5">
+      <div className="justify-self-center lg:hidden md:mt-6 mt-16 mx-5">
         <Image src="/img/plus.png" alt="plus" width={220} height={220} />
       </div>
 
       {/* 4 Points table*/}
-      <div className="grid grid-cols-2 gap-10 lg:hidden mt-6">
+      <div className="grid grid-cols-2 gap-10 lg:hidden md:mt-6 mt-18">
         {/* Kiri */}
         <div className="flex flex-col">
-          <div className="mt-5">
+          <div className="mt-5 md:h-auto h-28">
             <div className="flex flex-row gap-4 justify-end">
-              <div className="roboto text-lg text-blue-900 font-semibold text-right">
+              <div className="roboto md:text-lg text-md text-blue-900 font-semibold text-right">
                 <span>Real Time Kinematic</span>
               </div>
 
@@ -139,11 +139,12 @@ export default function Home() {
                 alt="target"
                 width={30}
                 height={30}
+                className="md:w-[30px] md:h-[30px] w-[20px] h-[20px]"
               />
             </div>
             <div className="grid grid-cols-3">
               <div></div>
-              <div className="col-span-2 text-sm text-gray-600 mt-1 font-semibold roboto text-right">
+              <div className="col-span-2 md:text-sm text-xs text-gray-600 mt-3 font-semibold roboto text-right leading-5 md:mt-4">
                 <span>
                   Generating more precision geospatial data than using standard
                   GPS.
@@ -154,7 +155,7 @@ export default function Home() {
 
           <div className="mt-5">
             <div className="flex flex-row gap-4 justify-end">
-              <div className="roboto text-lg text-blue-900 font-semibold text-right">
+              <div className="roboto md:text-lg text-md text-blue-900 font-semibold text-right">
                 <span>20MP Camera Lens</span>
               </div>
 
@@ -163,11 +164,12 @@ export default function Home() {
                 alt="camera"
                 width={30}
                 height={30}
+                className="md:w-[30px] md:h-[30px] w-[25px] h-[25px]"
               />
             </div>
             <div className="grid grid-cols-3">
               <div></div>
-              <div className="col-span-2 text-sm text-gray-600 mt-1 font-semibold roboto text-right">
+              <div className="col-span-2 md:text-sm text-xs text-gray-600 mt-3 font-semibold roboto text-right leading-5 md:mt-4">
                 <span>
                   By combining with RTK, it could producing a pin-sharp photo.
                 </span>
@@ -177,20 +179,21 @@ export default function Home() {
         </div>
 
         {/* Kanan */}
-        <div className="flex flex-col">
-          <div className="mt-5">
+        <div className="flex flex-col mr">
+          <div className="mt-5 md:h-auto h-28">
             <div className="flex flex-row gap-4">
               <Image
                 src="/img/hourglass.png"
                 alt="hourglass"
                 width={30}
                 height={30}
+                className="md:w-[30px] md:h-[30px] w-[25px] h-[25px]"
               />
-              <div className="roboto text-lg text-blue-900 font-semibold">
+              <div className="roboto md:text-lg text-md text-blue-900 font-semibold">
                 <span>Wide Flight Time and Range</span>
               </div>
             </div>
-            <div className="text-sm mr-40 text-gray-600 mt-1 font-semibold roboto">
+            <div className="md:text-sm text-xs mr-16 text-gray-600 mt-3 font-semibold roboto leading-5 md:mt-4">
               <span>
                 Suitable for huge project by flying within 1km<sup>2</sup> in 30
                 minutes with high accuration.
@@ -204,12 +207,13 @@ export default function Home() {
                 alt="sensor"
                 width={30}
                 height={30}
+                className="md:w-[30px] md:h-[30px] w-[25px] h-[25px]"
               />
-              <div className="roboto text-lg text-blue-900 font-semibold">
+              <div className="roboto md:text-lg text-md text-blue-900 font-semibold">
                 <span>Sensor of Obstacle Avoidance</span>
               </div>
             </div>
-            <div className="text-sm mr-40 justify-self-start text-gray-600 mt-1 font-semibold roboto">
+            <div className="md:text-sm text-xs mr-16 justify-self-start text-gray-600 mt-3 font-semibold roboto leading-5 md:mt-4">
               <span>
                 With 10 - 20 Hz detection speed, allowing to prevent from an
                 unpredictable obstacle while flying.
@@ -219,14 +223,14 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-blue-900 text-2xl roboto font-semibold text-center mt-28">
+      <div className="text-blue-900 md:text-2xl text-xl roboto font-semibold text-center md:mt-28 mt-32">
         <span>Our Clients Reviews</span>
       </div>
 
       {/* Card */}
-      <div className="mt-12 flex lg:justify-center items-center lg:flex-row flex-col mb-10 lg:gap-4 gap-8 mx-5">
+      <div className="md:mt-12 mt-20 flex lg:justify-center items-center lg:flex-row flex-col mb-10 lg:gap-10 md:gap-8 gap-16 mx-5">
         {/* Review */}
-        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 ">
+        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 lg:h-80 h-auto">
           {/* Konten Review Card */}
           <div className="flex flex-col mt-3">
             <div className="self-center">
@@ -247,12 +251,10 @@ export default function Home() {
                 className="rotate-180"
               />
             </div>
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4">
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4 lg:h-25 h-auto">
               <span>
-                Sebagai surveyor, akurasi adalah segalanya. Dengan RTK yang
-                presisi hingga tingkat sentimeter, saya bisa mengumpulkan data
-                topografi tanpa perlu koreksi manual yang memakan waktu.
-                Hasilnya? Pekerjaan lebih cepat dan efisien!
+                Previously, I had to point some coordinate manually. However right now, I could retrieve an accurate map using Phantom 4 RTK.
+                Thanks to the battery performance, It also help me to do a project planning.
               </span>
             </div>
 
@@ -266,17 +268,20 @@ export default function Home() {
             </div>
 
             <div className="roboto text-blue-custom font-semibold self-center text-sm mt-4">
-              <span>Sauqi Rizqullah</span>
+              <span>Budi Susanto</span>
             </div>
 
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center mb-2">
-              <span>Surveyor Lapangan</span>
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center">
+              <span>Project Contractor</span>
+            </div>
+            <div className="lg:hidden lg:mb-0 mb-2">
+
             </div>
           </div>
         </div>
 
         {/* Review */}
-        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 ">
+        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 lg:h-80 h-auto">
           {/* Konten Review Card */}
           <div className="flex flex-col mt-3">
             <div className="self-center">
@@ -297,12 +302,11 @@ export default function Home() {
                 className="rotate-180"
               />
             </div>
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4">
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4 lg:h-25 h-auto">
               <span>
-                Sebagai surveyor, akurasi adalah segalanya. Dengan RTK yang
-                presisi hingga tingkat sentimeter, saya bisa mengumpulkan data
-                topografi tanpa perlu koreksi manual yang memakan waktu.
-                Hasilnya? Pekerjaan lebih cepat dan efisien!
+                As a surveyor, accuration is everything. With RTK that precise until centimeter given,
+                I could collecting topography data without correcting manually which is wasting time.
+                Talk about the result? Done efficiently and faster.
               </span>
             </div>
 
@@ -319,14 +323,17 @@ export default function Home() {
               <span>Sauqi Rizqullah</span>
             </div>
 
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center mb-2">
-              <span>Surveyor Lapangan</span>
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center">
+              <span>Surveyor</span>
+            </div>
+            <div className="lg:hidden lg:mb-0 mb-2">
+
             </div>
           </div>
         </div>
 
         {/* Review */}
-        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 ">
+        <div className="outline-2 outline-offset-2 outline-gray-500/40 rounded-lg w-100 md:w-140 lg:w-72 lg:h-80 h-auto">
           {/* Konten Review Card */}
           <div className="flex flex-col mt-3">
             <div className="self-center">
@@ -347,12 +354,10 @@ export default function Home() {
                 className="rotate-180"
               />
             </div>
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4">
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-3 mx-4 text-center leading-5 lg:leading-4 lg:h-25 h-auto">
               <span>
-                Sebagai surveyor, akurasi adalah segalanya. Dengan RTK yang
-                presisi hingga tingkat sentimeter, saya bisa mengumpulkan data
-                topografi tanpa perlu koreksi manual yang memakan waktu.
-                Hasilnya? Pekerjaan lebih cepat dan efisien!
+                At the beginning, I was worried that I couldn't operate it, nevertheless it's interface is very intuitive and easy to use it.
+                The data is very accurate and the photo result quality is outstanding. Highly recommended for anyone who want to improve an efficiency of survey.
               </span>
             </div>
 
@@ -366,11 +371,14 @@ export default function Home() {
             </div>
 
             <div className="roboto text-blue-custom font-semibold self-center text-sm mt-4">
-              <span>Sauqi Rizqullah</span>
+              <span>Andrew Susanto</span>
             </div>
 
-            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center mb-2">
-              <span>Surveyor Lapangan</span>
+            <div className="text-gray-600/70 roboto font-semibold text-xs mt-1 mx-4 text-center">
+              <span>Junior GIS Technician</span>
+            </div>
+            <div className="lg:hidden lg:mb-0 mb-2">
+
             </div>
           </div>
         </div>
